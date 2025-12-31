53d39aa7ed519f29fdc0e5b9b73a26d3a3c8c6ad
Chinmay Soni <chinmaysoni@Chinmays-MacBook-Pro.local>
2025-12-31 18:05:07 +0530

    first commit

---
+++ auth/src/models/user.model.js
@@ -0,0 +1,42 @@
+const mongoose = require('mongoose')
+const { emit } = require('../app')
+
+
+const addressesSchema = new mongoose.Schema({
+    street:String,
+    city:String,
+    state:String,
+    zip:String,
+    country:String
+})
+const userSchema = new mongoose.Schema({
+    username:{
+        type:String,
+        required:true,
+        unique:true
+    },
+    email:{
+        type:String,
+        required:true,
+        unique:true
+    },
+    password:{
+        type:String
+    },
+    fullName:{
+        firstName:{type:String,required:true},
+        lastName:{type:String,required:true}
+    },
+    role:{
+        type:String,
+        enum:['user','seller'],
+        default: 'user'
+    },
+    addresses:[
+        addressesSchema
+    ]
+})
+
+const userModel = mongoose.model('user',userSchema)
+
+module.exports = userModel;
